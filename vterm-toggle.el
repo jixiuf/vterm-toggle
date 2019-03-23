4b9d3c72560de4062dcdee4278d846724d98bf8d
jixiufeng <jixiufeng@luojilab.com>
2019-03-24 00:32:23 +0800

    init vterm-toggle.el

---
+++ vterm-toggle.el
@@ -0,0 +1,187 @@
+;;; vterm-toggle.el --- Toggle to and from the vterm buffer
+
+;; Author: jixiuf  jixiuf@qq.com
+;; Keywords: vterm
+;; URL: https://github.com/jixiuf/vterm-toggle
+
+;; Copyright (C) 2019, jixiuf, all rights reserved.
+
+;; This file is NOT part of GNU Emacs.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; Description:
+;;
+;; Provides the command vterm-toggle which toggles between the
+;; vterm buffer and whatever buffer you are editing.
+;;
+;; This is done in an "intelligent" way.  Features are:
+;; o Starts a vterm if none is existent.
+;; o Minimum distortion of your window configuration.
+;; o When done in the vterm-buffer you are returned to the same window
+;;   configuration you had before you toggled to the shell.
+;; o If you desire, you automagically get a "cd" command in the shell to the
+;;   directory where your current buffers file exists; just call
+;;   vterm-toggle-cd instead of vterm-toggle.
+;;
+
+;;
+
+;;; Code:
+(require 'tramp)
+(require 'vterm)
+(require 'evil nil  t)
+
+
+(defvar vterm-toggle-configration nil)
+
+(defcustom vterm-toggle-evil-state-when-enter 'insert
+  "Default evil state for vterm buffer."
+  :group 'vterm-toggle
+  :type 'symbolp)
+(defcustom vterm-toggle-evil-state-when-leave 'normal
+  "Default evil state for vterm buffer."
+  :group 'vterm-toggle
+  :type 'symbolp)
+
+(defcustom vterm-toggle-prompt-regexp "^[a-zA-Z0-9_-]+@[^#$%\n]*[#$%] *"
+  "vterm prompt regexp. "
+  :group 'vterm-toggle
+  :type 'string)
+
+(defvar vterm-toggle-vterm-buffer-p-function 'vterm-toggle--default-vterm-mode-p
+  "Function to check whether a buffer is vterm-buffer mode. ")
+
+(defun vterm-toggle--default-vterm-mode-p(&optional args)
+  (derived-mode-p 'vterm-mode))
+
+
+(defun vterm-toggle-swith-evil-state (state)
+  (when (featurep 'evil)
+    (funcall (intern (format "evil-%S-state" state)))))
+
+;;;###autoload
+(defun vterm-toggle(&optional args)
+  (interactive "P")
+  (cond
+   ((funcall vterm-toggle-vterm-buffer-p-function args)
+    (vterm-toggle-hide))
+   (t
+    (vterm-toggle-show nil args))))
+
+;;;###autoload
+(defun vterm-toggle-cd(&optional args)
+  (interactive "P")
+  (cond
+   ((funcall vterm-toggle-vterm-buffer-p-function args)
+    (vterm-toggle-hide args))
+   (t
+    (vterm-toggle-show t args))))
+
+;;;###autoload
+(defun vterm-toggle-hide(&optional args)
+  (interactive "P")
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (funcall vterm-toggle-vterm-buffer-p-function args)
+        (vterm-toggle-swith-evil-state vterm-toggle-evil-state-when-leave)
+        (bury-buffer))))
+  (when vterm-toggle-configration
+    (set-window-configuration vterm-toggle-configration))
+  (when (funcall vterm-toggle-vterm-buffer-p-function args)
+    (switch-to-buffer (vterm-toggle--recent-other-buffer))))
+
+;;;###autoload
+(defun vterm-toggle-show(&optional  make-cd args)
+  (interactive)
+  (let* ((shell-buffer (vterm-toggle--recent-vterm-buffer make-cd args))
+         (dir (and make-cd
+                   (or list-buffers-directory default-directory)))
+         cd-cmd cur-host vterm-dir vterm-host)
+    (when make-cd
+      (if (ignore-errors (file-remote-p (or list-buffers-directory default-directory)))
+          (with-parsed-tramp-file-name (or list-buffers-directory default-directory) nil
+            (setq cur-host host)
+            (setq dir localname))
+        (setq cur-host (system-name)))
+      (setq cd-cmd (concat " cd " (shell-quote-argument dir))))
+    (if shell-buffer
+        (progn
+          (unless (funcall vterm-toggle-vterm-buffer-p-function args)
+            (setq vterm-toggle-configration (current-window-configuration)))
+          (pop-to-buffer shell-buffer)
+          (with-current-buffer shell-buffer
+            (when (derived-mode-p 'vterm-mode)
+              (if (ignore-errors (file-remote-p default-directory))
+                  (with-parsed-tramp-file-name default-directory nil
+                    (setq vterm-dir localname)
+                    (setq vterm-host host))
+                (setq vterm-dir default-directory)
+                (setq vterm-host (system-name)))
+              (when (and (not (equal vterm-dir dir))
+                         (equal vterm-host cur-host))
+                (vterm-send-key "u" nil nil t)
+                (vterm-send-string cd-cmd t)
+                (vterm-send-key "<return>" nil nil nil))))
+
+          (delete-other-windows))
+      (vterm)))
+  (vterm-toggle-swith-evil-state vterm-toggle-evil-state-when-enter))
+
+(defun vterm-toggle-skip-prompt ()
+  "Skip past the text matching regexp `vterm-toggle-prompt-regexp'.
+If this takes us past the end of the current line, don't skip at all."
+  (let ((eol (line-end-position)))
+    (when (and (looking-at vterm-toggle-prompt-regexp)
+	           (<= (match-end 0) eol))
+      (goto-char (match-end 0)))))
+
+(defun vterm-toggle-accept-cmd-p ()
+  (save-excursion
+    (goto-char (point-at-bol))
+    (vterm-toggle-skip-prompt)))
+
+
+(defun vterm-toggle--recent-vterm-buffer(&optional vterm-accept-cmd-p args)
+  (let ((shell-buffer))
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (funcall vterm-toggle-vterm-buffer-p-function args)
+          (cond
+           ((and (derived-mode-p 'vterm-mode)
+                 vterm-accept-cmd-p)
+            (when (vterm-toggle-accept-cmd-p)
+              (unless shell-buffer
+                (setq shell-buffer buf))))
+           (t
+            (unless shell-buffer
+              (setq shell-buffer buf)))))))
+    shell-buffer))
+
+(defun vterm-toggle--recent-other-buffer(&optional args)
+  (let ((list (buffer-list))
+        (index 0)
+        shell-buffer buf )
+    (cl-loop until shell-buffer do
+             (setq buf (nth index list))
+             (with-current-buffer buf
+               (when (and (not (funcall vterm-toggle-vterm-buffer-p-function args))
+                          (not (char-equal ?\  (aref (buffer-name) 0))))
+                 (setq shell-buffer buf)))
+             (setq index (1+ index)))
+    shell-buffer))
+
+(provide 'vterm-toggle)
+
+;; Local Variables:
+;; coding: utf-8
+;; End:
+
+;;; vterm-toggle.el ends here.
