2258eb19e8bde75f79505c4306f3476bcedce56c
jixiuf <jixiuf@qq.com>
2021-06-29 17:27:54 +0800

    fixup

--- vterm-toggle.el
+++ vterm-toggle.el
@@ -48,7 +48,6 @@
 (declare-function vterm-send-string "vterm")
 (declare-function vterm-send-return "vterm")
 (declare-function vterm-other-window "vterm")
-(declare-variable vterm-other-window "vterm")
 (defvar vterm-buffer-name  "*vterm*")
 
 (defcustom vterm-toggle-show-hook nil
